c8f881c6ebd3850afd80e66ca0bfb1b56f2575f7
tjjoris <tjjoris@gmail.com>
2026-07-29 16:13:00 -0400

    App.php echoes the url value of the url to the screen if it has one.

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,23 @@
 <?php
 
 class App {
+	//default controller and method run when boostratpping application.
+	protected $controller = 'home';
+	protected $method = 'index';
+	protected $params = [];
+
 	public function __construct() {
-		echo "hello world";
+		$this->parseUrl();
+	}
+
+	/*
+	 *gets the url value and echoes it to the screen. for example if i visit:
+	 http://localhost/php/mvc/public/?url=something
+	I see: "somthing"
+	 */
+	public function parseUrl() {
+		if(isset($_GET['url'])) {
+			echo $_GET['url'];
+		}
 	}
 }
