bb8fdf4ba9b872bb657b8806461106117a7c86b6
tjjoris <tjjoris@gmail.com>
2026-07-29 14:36:41 -0400

    started creating file structure, appears to be working.

---
+++ app/init.php
@@ -0,0 +1,5 @@
+<?php
+
+//require two core classes needed
+require_once 'core/App.php';
+require_once 'core/Controller.php';
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+//requires the initialization file which requires all the componetnts I n eed.
+require_once "../app/init.php";
+
+$app = new App;
