bb4aea4f1724c73b851de04cda674d3d1050919e
tjjoris <tjjoris@gmail.com>
2026-07-29 16:59:09 -0400

    exploding url in App.php so it can be broken into the controller and function parts.

--- app/core/App.php
+++ app/core/App.php
@@ -4,10 +4,11 @@ class App {
 	//default controller and method run when boostratpping application.
 	protected $controller = 'home';
 	protected $method = 'index';
+	//params from parsed url.
 	protected $params = [];
 
 	public function __construct() {
-		$this->parseUrl();
+		print_r($this->parseUrl());
 	}
 
 	/*
@@ -17,7 +18,12 @@ class App {
 	 */
 	public function parseUrl() {
 		if(isset($_GET['url'])) {
-			echo $_GET['url'];
+			//sanitize the url, split it up to see if a controller has been accessed or method and parameters passed in.
+			//trim because if it has a trailing slash it will cause another elemnt to the array when we explode it.
+			//it will automatically trim white space form the right but if we provide a character it will also trim that.
+			//we use filter_var to sanitize it then FILTER_SANITIZE_URL
+			//we also explode it by a forward slash which is why the trailing slash is removed. the first character is what you explode by.
+			return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
 		}
 	}
 }
