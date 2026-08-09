aab819260e76bbcf78123e19a793477f74435dd5
tjjoris <tjjoris@gmail.com>
2026-08-09 17:39:35 -0400

    successfully connected to local database using local .env file and database.php.

--- app/database.php
+++ app/database.php
@@ -12,6 +12,7 @@ class Database {
 	public function getDb() {
 		$db_host = $_ENV['DB_HOST'];
 		echo "DB host " . $db_host;
+		$this->connect();
 	}
 
 	public function connect() {
@@ -20,9 +21,17 @@ class Database {
 		$db_user = $_ENV['DB_USERNAME'];
 		$db_pass = $_ENV['DB_PASSWORD'];
 		$db_port = $_ENV['DB_PORT'];
-		//try {
-			//$this->db
-			//
-		//}
+		try {
+			$this->db = new PDO(
+				"mysql:host=$db_host;
+				dbname=$db_name;
+				charset=utf8mb4",
+				$db_user,
+				$db_pass
+			);
+				echo "connected!";
+		} catch (PDOException $e) {
+			die("Database Error: " . $e->getMessage());
+		}
 	}
 }
