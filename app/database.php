5f1a43534ae47c4ffbdf2575aa0c0860998f5278
tjjoris <tjjoris@gmail.com>
2026-08-09 17:31:36 -0400

    loading local variables for database connection.

--- app/database.php
+++ app/database.php
@@ -7,8 +7,22 @@
 
 class Database {
 
+	private $db;
+
 	public function getDb() {
 		$db_host = $_ENV['DB_HOST'];
 		echo "DB host " . $db_host;
+	}
+
+	public function connect() {
+		$db_host = $_ENV['DB_HOST'];
+		$db_name = $_ENV['DB_DATABASE'];
+		$db_user = $_ENV['DB_USERNAME'];
+		$db_pass = $_ENV['DB_PASSWORD'];
+		$db_port = $_ENV['DB_PORT'];
+		//try {
+			//$this->db
+			//
+		//}
 	}
 }
