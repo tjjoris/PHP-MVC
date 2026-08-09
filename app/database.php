fdf7b817dfd2afb1836bf8f4ff02bc5a27c0c34a
tjjoris <tjjoris@gmail.com>
2026-08-09 16:33:16 -0400

    added beginning database script, untested, probably won't work.

---
+++ app/database.php
@@ -0,0 +1,16 @@
+/*
+ * database.php
+ * @Author: Luke Johnson
+ * loads .env files and connects to database.
+ */
+
+<?php
+class Database {
+
+	//use Dotenv\Dotenv;
+
+	public function getDb() {
+		$db_host = $_ENV['DB_HOST'];
+		echo "DB host " . $db_host;
+	}
+}
