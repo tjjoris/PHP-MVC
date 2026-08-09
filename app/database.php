6884343bae95b9c5829a1e5e0ec21d0cecb82add
tjjoris <tjjoris@gmail.com>
2026-08-09 17:14:36 -0400

    in process of loading .env in init.php.

--- app/database.php
+++ app/database.php
@@ -7,8 +7,6 @@
 
 class Database {
 
-//	use Dotenv\Dotenv;
-
 	public function getDb() {
 		$db_host = $_ENV['DB_HOST'];
 		echo "DB host " . $db_host;
--- app/init.php
+++ app/init.php
@@ -1,7 +1,13 @@
 <?php
+//init.php
+require_once __DIR__ . '/../vendor/autoload.php';
+//use Dotenv\Dotenv;
+
+//load .env once application starts.
+//$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+//$dotenv->load();
 
 //require two core classes needed
-require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/core/App.php';
 require_once __DIR__ . '/core/Controller.php';
 require_once __DIR__ . '/models/Model.php';
