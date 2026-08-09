11f3f31ab801c148082a251d91a4372dd9c57eab
tjjoris <tjjoris@gmail.com>
2026-08-09 17:15:40 -0400

    init.php loads .env and parser is used in database to get hidden DB host address and print it to screen.

--- app/init.php
+++ app/init.php
@@ -1,11 +1,11 @@
 <?php
 //init.php
 require_once __DIR__ . '/../vendor/autoload.php';
-//use Dotenv\Dotenv;
+use Dotenv\Dotenv;
 
 //load .env once application starts.
-//$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-//$dotenv->load();
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 //require two core classes needed
 require_once __DIR__ . '/core/App.php';
