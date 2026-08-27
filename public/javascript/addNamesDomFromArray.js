4805537353928daf00389aa09eed7d44fdc270b7
tjjoris <tjjoris@gmail.com>
2026-08-27 15:51:26 -0400

    added header comment to addNamesDomFromArray.js as a reminder of importing javascript files.

--- public/javascript/addNamesDomFromArray.js
+++ public/javascript/addNamesDomFromArray.js
@@ -3,6 +3,7 @@
  Author: Luke Johnson
  takes an array of names and creates a DOM element for each name.
 this is a php file using javascript code.
+As a note, the javascript file had to be put in the public folder to be accessed by the view, and the view needed to use a path starting at the root.
  */
 
 export function AddNamesDomFromArray() {
